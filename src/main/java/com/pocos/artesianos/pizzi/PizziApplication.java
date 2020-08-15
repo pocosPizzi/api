2d92b24eaa54b0b9228c3cce3741b9ee58ebe022
pocosPizzi <pocosartesianospizzi@gmail.com>
2020-08-14 22:21:27 -0300

    create: project base

---
+++ src/main/java/com/pocos/artesianos/pizzi/PizziApplication.java
@@ -0,0 +1,13 @@
+package com.pocos.artesianos.pizzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizziApplication.class, args);
+	}
+
+}
