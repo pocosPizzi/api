c3d3e7cad2270ec293a23ed0536b7a8679e4cfe0
pocosPizzi <pocosartesianospizzi@gmail.com>
2020-08-19 21:37:22 -0300

    refactor:

---
+++ src/main/java/com/pocos/artesianos/pizzi/config/CorsConfig.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.pocos.artesianos.pizzi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author cleomar
+ *
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
